4fa37ba8e3b74e4af2c55180a258e8ecad55012e
Klimko Dmitrii <DKlimko@avectis.local>
2022-10-21 15:20:23 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import { useEffect } from 'react';
+import './App.css';
+const tg = window.Telegram.WebApp;
+
+const App = () => {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  }
+  return (
+    <div className="App">
+      Ghbdtn
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
