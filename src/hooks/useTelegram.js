5df323bca60629136063e9c42ade5b39f44facec
Klimko Dmitrii <DKlimko@avectis.local>
2022-11-05 08:07:01 +0300

    1.2

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -18,6 +18,7 @@ export const useTelegram = () => {
         tg,
         onClose,
         onToggleButton,
-        user: tg.initDataUnsafe?.user
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     });
 };
